fc56f261fca7b30a25f827b1b8142829ab491ed8
Elad Aviczer <elad492@gmail.com>
2022-11-24 10:28:04 +0200

    disabled local exec

--- modules/ec2_instances/main.tf
+++ modules/ec2_instances/main.tf
@@ -35,26 +35,26 @@ resource "aws_instance" "devops" {
   }
 
 
-  provisioner "file" {
-    source      = var.script_operation[0]
-    destination = var.script_operation[1]
-  }
-  # Change permissions on bash script and execute from ec2-user.
-  provisioner "remote-exec" {
-    inline = [
-      var.script_operation[2],
-      var.script_operation[3]
-    ]
-  }
+  # provisioner "file" {
+  #   source      = var.script_operation[0]
+  #   destination = var.script_operation[1]
+  # }
+  # # Change permissions on bash script and execute from ec2-user.
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     var.script_operation[2],
+  #     var.script_operation[3]
+  #   ]
+  # }
 
-  # Login to the ec2-user with the aws key.
-  connection {
-    type = "ssh"
-    user = "ubuntu"
-    # private_key = file("${var.generated_key_name}.pem")
-    private_key = tls_private_key.dev_key.private_key_pem
-    host        = self.private_ip
-  }
+  # # Login to the ec2-user with the aws key.
+  # connection {
+  #   type = "ssh"
+  #   user = "ubuntu"
+  #   # private_key = file("${var.generated_key_name}.pem")
+  #   private_key = tls_private_key.dev_key.private_key_pem
+  #   host        = self.private_ip
+  # }
 
   tags = {
     Name        = "${var.instances_name}${count.index}"
